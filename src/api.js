a4ddf512d6400a812bd30cf4056e560f6c18119c
maxmay94 <maxmay94@gmail.com>
2023-05-09 17:46:57 -0400

    changes to api.js

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import Constants from 'expo-constants'
 
 const api = axios.create({
+  // baseURL: `http://127.0.01:8000/api/`,
   baseURL: `http://${Constants.manifest.debuggerHost.split(':').shift()}:8000/api/`,
 })
 
